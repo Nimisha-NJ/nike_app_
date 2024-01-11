115245431eddb7b0a66ccb670f12f1a6d57099ea
Salini <salini.ks@altd.in>
2024-01-11 15:04:50 +0530

    UPDATED  project

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,14 @@
-
-const Button = ({label , iconUrl}) => {
+const Button = ({ label, iconUrl }) => {
   return (
-    <button className=" flex justify-center items-center gap-2 text-lg font-montserrat px-7 py-4 leading-none bg-coral-red rounded-full text-white">{label}
-     <img src={iconUrl} alt="arrow rigt icon" className=" ml-2 rounded-full w-5 h-5"/>
+    <button className=" flex justify-center items-center gap-2 text-lg font-montserrat px-7 py-4 leading-none bg-coral-red rounded-full text-white">
+      {label}
+     {iconUrl &&  <img
+        src={iconUrl}
+        alt="arrow rigt icon"
+        className=" ml-2 rounded-full w-5 h-5"
+      />}
     </button>
-   
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
